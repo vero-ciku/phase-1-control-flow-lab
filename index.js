6fc95bcc86628c6e47c2de69a66bd633cfee0d1b
Veronicah <veronicah.gicheru@student.moringaschool.com>
2022-06-07 01:46:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet()
+  if (ride is <=400 feet) {
+  this one is on me 
+} else (charges 30 dollars for a distance over 2000 feet) {
+  I will gladly take your thirty bucks
+} else if (does not allow rides over 2500 feet) {
+   no can do 
 }
 
 function ternaryCheckCity(){
